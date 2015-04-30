7b7f98dbb883d1955c15f60aa59eb60934de4681
Uri Goldstein <uri.goldstein@gmail.com>
2015-04-30 23:13:49 +0300

    Initial commit

---
+++ src/twitter-muter/test/muting.js
@@ -0,0 +1,12 @@
+﻿var expect = require("chai").expect;
+var muting = require('./muting');
+
+describe("muting", function () {
+    describe("init()", function () {
+        it("should inject an object into twitter", function () {
+            var twitter = "twitter";
+            muting.init(twitter);
+            expect(muting.twitter).to.equal(twitter);
+        });
+    });
+});
---
+++ src/twitter-muter/test/testMuting.js
@@ -0,0 +1,21 @@
+﻿var expect = require("chai").expect;
+var muting = require("../muting");
+
+describe("muting", function () {
+    describe("init()", function () {
+        it("should inject an object into twitter", function () {
+            var twitter = "twitter";
+            muting.init(twitter);
+            expect(muting.twitter).to.equal(twitter);
+        });
+    });
+
+    describe("processDirectMessage()", function () {
+        it("should inject an object into twitter", function () {
+            var twitter = "twitter";
+            muting.init(twitter);
+            expect(muting.processDirectMessage("fakeurig 1h")).to.deep.equal(["fakeurig", "1h"]);
+        });
+    });
+
+});
